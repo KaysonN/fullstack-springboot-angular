34e05402a40e0d5560f9bc651c55ce930f64b906
KaysonN <carloskayson@hotmail.com>
2023-03-15 11:43:43 -0300

    Criação do backend e mudança de IDE devido a conflitos

---
+++ backend/src/main/java/com/kayson/CRUDSpring/CrudSpringApplication.java
@@ -0,0 +1,14 @@
+package com.kayson.CRUDSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApplication.class, args);
+		System.out.println("Oi");
+	}
+
+}
